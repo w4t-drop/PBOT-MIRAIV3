ddd3de550b7c89a04691891e7d22fcb4eb1089fb
w4t-drop <nguoitothanoi@gmail.com>
2025-03-13 23:44:51 +0700

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 
 const FACEBOOK_URL = 'https://www.facebook.com/';
 const COOKIE_FILE = 'cookie.txt';
-const FB_EMAIL = 'your_email'; // Thay bằng email đăng nhập
-const FB_PASSWORD = 'your_password'; // Thay bằng mật khẩu
+const FB_EMAIL = '61574219153979'; // Thay bằng email đăng nhập
+const FB_PASSWORD = 'abc123@'; // Thay bằng mật khẩu
 
 async function getFacebookCookie() {
     const browser = await puppeteer.launch({
